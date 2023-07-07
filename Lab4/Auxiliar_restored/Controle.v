adb6fa9e5c758b73102501b869427e5bc361543e
leorb99 <leo.rb1999@gmail.com>
2023-07-07 13:53:33 -0300

    update controle

--- Lab4/Auxiliar_restored/Controle.v
+++ Lab4/Auxiliar_restored/Controle.v
@@ -5,12 +5,12 @@
 module Controle (
 	input wire iCLK, iRST,
 	input wire [6:0] iOpCode,
-	output wire [1:0] oMem2Reg, oOrigAULA, oOrigBULA, oALUOp,
-	output wire oOrigPC, oEscrevePCB, oEscreveReg, oEscreveIR, oLeMem, oEscreveMem, oIouD, oEscrevePC, oEscrevePCCond
+	output reg [1:0] oMem2Reg, oOrigAULA, oOrigBULA, oALUOp,
+	output reg oOrigPC, oEscrevePCB, oEscreveReg, oEscreveIR, oLeMem, oEscreveMem, oIouD, oEscrevePC, oEscrevePCCond
 );
 reg [3:0] estado;
 parameter inicio 				= 4'b0000,
-			 decod 		= 4'b0001,
+			 decod 				= 4'b0001,
 			 load_store 		= 4'b0010,
 			 load 				= 4'b0011,
 			 conclusao_lw 		= 4'b0100,
@@ -25,45 +25,53 @@ initial
 		estado <= inicio;
 	end
 
-always @(posedge iRST or posedge iCLK)
+always @(posedge iCLK, posedge iRST)
 	begin
-		if(iRST)
+		if(iRST == 1'b1)
 			estado <= inicio;
 		else
-			case(estado)
-				inicio:
-					estado <= decod;
-				decod:
-					
-						case(iOpCode)
-							OPC_LOAD:
-								estado <= load_store;
-							OPC_STORE:
-								estado <= load_store;
-							OPC_RTYPE:
-								estado <= tipo_r;
-							OPC_BRANCH:
-								estado <= beq;
-							OPC_JAL:
-								estado <= jal;
-						endcase
-					
-				load_store:
-					begin
-						case(iOpCode)
-							OPC_LOAD:
-								estado <= load;
-							OPC_STORE:
-								estado <= store;
-						endcase	
-					end
-				load:
-					estado <= conclusao_lw;
-				tipo_r:
-					estado <= conclusao_tipo_r;
-				default:
-					estado <= inicio;
+			begin
+				case(estado)
+					inicio:
+						begin
+							estado <= decod;
+						end
+					decod:
+						begin
+							case(iOpCode)
+								OPC_LOAD, OPC_STORE:
+									estado <= load_store;
+								OPC_RTYPE:
+									estado <= tipo_r;
+								OPC_BRANCH:
+									estado <= beq;
+								OPC_JAL:
+									estado <= jal;
+							endcase
+						end
+					load_store:
+						begin
+							case(iOpCode)
+								OPC_LOAD:
+									estado <= load;
+								OPC_STORE:
+									estado <= store;
+							endcase	
+						end
+					load:
+						begin
+							estado <= conclusao_lw;
+						end
+					tipo_r:
+						begin
+							estado <= conclusao_tipo_r;
+						end
+					default:
+						begin
+							estado <= inicio;
+						end
 			endcase
+		end
 	end
 	
 always @(estado)
@@ -71,179 +79,179 @@ always @(estado)
 		case(estado)
 			inicio:
 				begin
-					oMem2Reg 		<= 2'bxx;
-					oOrigAULA 		<= 2'b10;
-					oOrigBULA 		<= 2'b01;
-					oALUOp 			<= 2'b00;
-					oOrigPC 			<= OFF;
-					oEscrevePCB 	<= ON;
-					oEscreveReg 	<= OFF;
-					oEscreveIR 		<= ON;
-					oLeMem 			<= ON;
-					oEscreveMem 	<= OFF;
-					oIouD 			<= OFF;
-					oEscrevePC 		<= ON;
-					oEscrevePCCond <= OFF;
+					oMem2Reg 		= 2'b00;
+					oOrigAULA 		= 2'b10;
+					oOrigBULA 		= 2'b01;
+					oALUOp 			= 2'b00;
+					oOrigPC 			= OFF;
+					oEscrevePCB 	= ON;
+					oEscreveReg 	= OFF;
+					oEscreveIR 		= ON;
+					oLeMem 			= ON;
+					oEscreveMem 	= OFF;
+					oIouD 			= OFF;
+					oEscrevePC 		= ON;
+					oEscrevePCCond = OFF;
 				end
 			decod:
 				begin
-					oMem2Reg 		<= 2'bxx;
-					oOrigAULA 		<= 2'b00;
-					oOrigBULA 		<= 2'b11;
-					oALUOp 			<= 2'b00;
-					oOrigPC 			<= OFF;
-					oEscrevePCB 	<= OFF;
-					oEscreveReg 	<= OFF;
-					oEscreveIR 		<= OFF;
-					oLeMem 			<= OFF;
-					oEscreveMem 	<= OFF;
-					oIouD 			<= OFF;
-					oEscrevePC 		<= OFF;
-					oEscrevePCCond <= OFF;
+					oMem2Reg 		= 2'b00;
+					oOrigAULA 		= 2'b00;
+					oOrigBULA 		= 2'b11;
+					oALUOp 			= 2'b00;
+					oOrigPC 			= OFF;
+					oEscrevePCB 	= OFF;
+					oEscreveReg 	= OFF;
+					oEscreveIR 		= OFF;
+					oLeMem 			= OFF;
+					oEscreveMem 	= OFF;
+					oIouD 			= OFF;
+					oEscrevePC 		= OFF;
+					oEscrevePCCond = OFF;
 				end
 			load_store:
 				begin
-					oMem2Reg 		<= 2'bxx;
-					oOrigAULA 		<= 2'b01;
-					oOrigBULA 		<= 2'b10;
-					oALUOp 			<= 2'b00;
-					oOrigPC 			<= OFF;
-					oEscrevePCB 	<= OFF;
-					oEscreveReg 	<= OFF;
-					oEscreveIR 		<= OFF;
-					oLeMem 			<= OFF;
-					oEscreveMem 	<= OFF;
-					oIouD 			<= OFF;
-					oEscrevePC 		<= OFF;
-					oEscrevePCCond <= OFF;
+					oMem2Reg 		= 2'b00;
+					oOrigAULA 		= 2'b01;
+					oOrigBULA 		= 2'b10;
+					oALUOp 			= 2'b00;
+					oOrigPC 			= OFF;
+					oEscrevePCB 	= OFF;
+					oEscreveReg 	= OFF;
+					oEscreveIR 		= OFF;
+					oLeMem 			= OFF;
+					oEscreveMem 	= OFF;
+					oIouD 			= OFF;
+					oEscrevePC 		= OFF;
+					oEscrevePCCond = OFF;
 				end			
 			load:
 				begin
-					oMem2Reg 		<= 2'bxx;
-					oOrigAULA 		<= 2'bxx;
-					oOrigBULA 		<= 2'bxx;
-					oALUOp 			<= 2'bxx;
-					oOrigPC 			<= OFF;
-					oEscrevePCB 	<= OFF;
-					oEscreveReg 	<= OFF;
-					oEscreveIR 		<= OFF;
-					oLeMem 			<= ON;
-					oEscreveMem 	<= OFF;
-					oIouD 			<= ON;
-					oEscrevePC 		<= OFF;
-					oEscrevePCCond <= OFF;
+					oMem2Reg 		= 2'b00;
+					oOrigAULA 		= 2'b00;
+					oOrigBULA 		= 2'b00;
+					oALUOp 			= 2'b00;
+					oOrigPC 			= OFF;
+					oEscrevePCB 	= OFF;
+					oEscreveReg 	= OFF;
+					oEscreveIR 		= OFF;
+					oLeMem 			= ON;
+					oEscreveMem 	= OFF;
+					oIouD 			= ON;
+					oEscrevePC 		= OFF;
+					oEscrevePCCond = OFF;
 				end			
 			conclusao_lw:
 				begin
-					oMem2Reg 		<= 2'b10;
-					oOrigAULA 		<= 2'bxx;
-					oOrigBULA 		<= 2'bxx;
-					oALUOp 			<= 2'bxx;
-					oOrigPC 			<= OFF;
-					oEscrevePCB 	<= OFF;
-					oEscreveReg 	<= ON;
-					oEscreveIR 		<= OFF;
-					oLeMem 			<= OFF;
-					oEscreveMem 	<= OFF;
-					oIouD 			<= OFF;
-					oEscrevePC 		<= OFF;
-					oEscrevePCCond <= OFF;
+					oMem2Reg 		= 2'b10;
+					oOrigAULA 		= 2'b00;
+					oOrigBULA 		= 2'b00;
+					oALUOp 			= 2'b00;
+					oOrigPC 			= OFF;
+					oEscrevePCB 	= OFF;
+					oEscreveReg 	= ON;
+					oEscreveIR 		= OFF;
+					oLeMem 			= OFF;
+					oEscreveMem 	= OFF;
+					oIouD 			= OFF;
+					oEscrevePC 		= OFF;
+					oEscrevePCCond = OFF;
 				end			
 			store:
 				begin
-					oMem2Reg 		<= 2'bxx;
-					oOrigAULA 		<= 2'bxx;
-					oOrigBULA 		<= 2'bxx;
-					oALUOp 			<= 2'bxx;
-					oOrigPC 			<= OFF;
-					oEscrevePCB 	<= OFF;
-					oEscreveReg 	<= OFF;
-					oEscreveIR 		<= OFF;
-					oLeMem 			<= OFF;
-					oEscreveMem 	<= ON;
-					oIouD 			<= ON;
-					oEscrevePC 		<= OFF;
-					oEscrevePCCond <= OFF;
+					oMem2Reg 		= 2'b00;
+					oOrigAULA 		= 2'b00;
+					oOrigBULA 		= 2'b00;
+					oALUOp 			= 2'b00;
+					oOrigPC 			= OFF;
+					oEscrevePCB 	= OFF;
+					oEscreveReg 	= OFF;
+					oEscreveIR 		= OFF;
+					oLeMem 			= OFF;
+					oEscreveMem 	= ON;
+					oIouD 			= ON;
+					oEscrevePC 		= OFF;
+					oEscrevePCCond = OFF;
 				end			
 			tipo_r:
 				begin
-					oMem2Reg 		<= 2'bxx;
-					oOrigAULA 		<= 2'b01;
-					oOrigBULA 		<= 2'b00;
-					oALUOp 			<= 2'b10;
-					oOrigPC 			<= OFF;
-					oEscrevePCB 	<= OFF;
-					oEscreveReg 	<= OFF;
-					oEscreveIR 		<= OFF;
-					oLeMem 			<= OFF;
-					oEscreveMem 	<= OFF;
-					oIouD 			<= OFF;
-					oEscrevePC 		<= OFF;
-					oEscrevePCCond <= OFF;
+					oMem2Reg 		= 2'b00;
+					oOrigAULA 		= 2'b01;
+					oOrigBULA 		= 2'b00;
+					oALUOp 			= 2'b10;
+					oOrigPC 			= OFF;
+					oEscrevePCB 	= OFF;
+					oEscreveReg 	= OFF;
+					oEscreveIR 		= OFF;
+					oLeMem 			= OFF;
+					oEscreveMem 	= OFF;
+					oIouD 			= OFF;
+					oEscrevePC 		= OFF;
+					oEscrevePCCond = OFF;
 				end			
 			conclusao_tipo_r:
 				begin
-					oMem2Reg 		<= 2'b00;
-					oOrigAULA 		<= 2'bxx;
-					oOrigBULA 		<= 2'bxx;
-					oALUOp 			<= 2'bxx;
-					oOrigPC 			<= OFF;
-					oEscrevePCB 	<= OFF;
-					oEscreveReg 	<= ON;
-					oEscreveIR 		<= OFF;
-					oLeMem 			<= OFF;
-					oEscreveMem 	<= OFF;
-					oIouD 			<= OFF;
-					oEscrevePC 		<= OFF;
-					oEscrevePCCond <= OFF;
+					oMem2Reg 		= 2'b00;
+					oOrigAULA 		= 2'b00;
+					oOrigBULA 		= 2'b00;
+					oALUOp 			= 2'b00;
+					oOrigPC 			= OFF;
+					oEscrevePCB 	= OFF;
+					oEscreveReg 	= ON;
+					oEscreveIR 		= OFF;
+					oLeMem 			= OFF;
+					oEscreveMem 	= OFF;
+					oIouD 			= OFF;
+					oEscrevePC 		= OFF;
+					oEscrevePCCond = OFF;
 				end			
 			beq:
 				begin
-					oMem2Reg 		<= 2'bxx;
-					oOrigAULA 		<= 2'b01;
-					oOrigBULA 		<= 2'b00;
-					oALUOp 			<= 2'b01;
-					oOrigPC 			<= ON;
-					oEscrevePCB 	<= OFF;
-					oEscreveReg 	<= OFF;
-					oEscreveIR 		<= OFF;
-					oLeMem 			<= OFF;
-					oEscreveMem 	<= OFF;
-					oIouD 			<= OFF;
-					oEscrevePC 		<= OFF;
-					oEscrevePCCond <= ON;
+					oMem2Reg 		= 2'b00;
+					oOrigAULA 		= 2'b01;
+					oOrigBULA 		= 2'b00;
+					oALUOp 			= 2'b01;
+					oOrigPC 			= ON;
+					oEscrevePCB 	= OFF;
+					oEscreveReg 	= OFF;
+					oEscreveIR 		= OFF;
+					oLeMem 			= OFF;
+					oEscreveMem 	= OFF;
+					oIouD 			= OFF;
+					oEscrevePC 		= OFF;
+					oEscrevePCCond = ON;
 				end			
 			jal:
 				begin
-					oMem2Reg 		<= 2'b01;
-					oOrigAULA 		<= 2'bxx;
-					oOrigBULA 		<= 2'bxx;
-					oALUOp 			<= 2'bxx;
-					oOrigPC 			<= ON;
-					oEscrevePCB 	<= OFF;
-					oEscreveReg 	<= ON;
-					oEscreveIR 		<= OFF;
-					oLeMem 			<= OFF;
-					oEscreveMem 	<= OFF;
-					oIouD 			<= OFF;
-					oEscrevePC 		<= ON;
-					oEscrevePCCond <= OFF;
+					oMem2Reg 		= 2'b01;
+					oOrigAULA 		= 2'b00;
+					oOrigBULA 		= 2'b00;
+					oALUOp 			= 2'b00;
+					oOrigPC 			= ON;
+					oEscrevePCB 	= OFF;
+					oEscreveReg 	= ON;
+					oEscreveIR 		= OFF;
+					oLeMem 			= OFF;
+					oEscreveMem 	= OFF;
+					oIouD 			= OFF;
+					oEscrevePC 		= ON;
+					oEscrevePCCond = OFF;
 				end			
 			default:
 				begin
-					oMem2Reg 		<= 2'b00;
-					oOrigAULA 		<= 2'b00;
-					oOrigBULA 		<= 2'b00;
-					oALUOp 			<= 2'b00;
-					oOrigPC 			<= OFF;
-					oEscrevePCB 	<= OFF;
-					oEscreveReg 	<= OFF;
-					oEscreveIR 		<= OFF;
-					oLeMem 			<= OFF;
-					oEscreveMem 	<= OFF;
-					oIouD 			<= OFF;
-					oEscrevePC 		<= OFF;
-					oEscrevePCCond <= OFF;
+					oMem2Reg 		= 2'b00;
+					oOrigAULA 		= 2'b00;
+					oOrigBULA 		= 2'b00;
+					oALUOp 			= 2'b00;
+					oOrigPC 			= OFF;
+					oEscrevePCB 	= OFF;
+					oEscreveReg 	= OFF;
+					oEscreveIR 		= OFF;
+					oLeMem 			= OFF;
+					oEscreveMem 	= OFF;
+					oIouD 			= OFF;
+					oEscrevePC 		= OFF;
+					oEscrevePCCond = OFF;
 				end
 		endcase
 	end	
